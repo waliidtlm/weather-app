cfca0b5a932671bf7a41273be3486de744d303ba
walidtlm <tlemcani.waliid@gmail.com>
2024-01-05 23:02:09 +0100

    change weather icon

--- weather app/src/App.jsx
+++ weather app/src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react'
 import React from 'react'
 import axios from "axios"
 import './style.css'
-
-
+import { BsCloudsFill } from "react-icons/bs";
+import { IoRainyOutline } from "react-icons/io5";
+import { BsClouds } from "react-icons/bs";
+import { TiWeatherSunny } from "react-icons/ti";
+import { IoIosSnow } from "react-icons/io";
 
 function App() {
   const [data, setData] = useState({})
@@ -19,9 +22,9 @@ function App() {
     let year = d.getFullYear();
 
     return  `${day} ${date} ${month} ${year}`
+  }
 
   
-  }
 
   const api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=99f0591a5302fc3b51e9686f8c4e7047`
   
@@ -34,48 +37,72 @@ function App() {
       setLocation('')
     }
   }
+  const renderWeatherIcon = () => {
+    if (data.weather) {
+      const currentWeather = data.weather[0].main
+      switch(currentWeather) {
+        case "Clouds":
+          return <BsClouds size={150} />;
+        case "Clear": 
+          return <TiWeatherSunny size={150}/>;
+        case "Rain":
+          return <IoRainyOutline size={150} />;
+        case "Snow":
+          return <IoIosSnow size={150}/>;
+        default:
+          return null
+      }
+    }
+
+  }
   return (
     <div className="app">
       <div className="search">
-      <input
-        type="text"
-        placeholder="Enter Location"
-        maxLength={20}
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
-        onKeyDown={searchLocation}
-      />
+        <input
+          type="text"
+          placeholder="Enter Location"
+          maxLength={20}
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyDown={searchLocation}
+        />
       </div>
       <div className="container">
         <div className="top">
           <div className="location">
-           <h2>{data.name}</h2>
+            <h2>{data.name}</h2>
+            {data.sys ? <h3>{data.sys.country}</h3> : null}
           </div>
           <div className="temp">
-           {data.main ? <h1>{data.main.temp.toFixed()}℃</h1>: null }
+            {data.main ? <h1>{data.main.temp.toFixed()}℃</h1> : null}
           </div>
           <div className="date">
             <p>{dateBuilder(new Date())}</p>
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
-            
+          </div>
+          <div className="weather-icon">
+            {renderWeatherIcon()}
+          </div>
+          <div className="details">
+            {data.weather ? <p>{data.weather[0].description}</p> : null}
           </div>
         </div>
 
         <div className="bottom">
           <div className="feels">
-            {data.main ? <p>{data.main.feels_like.toFixed()}℃</p> : ('_')}
-            
+            {data.main ? <p>{data.main.feels_like.toFixed()}℃</p> : "_"}
+
             <p>Feels Like</p>
           </div>
           <div className="humidity">
-            {data.main ? <p>{data.main.humidity}%</p> : ('_')}
-            
+            {data.main ? <p>{data.main.humidity}%</p> : "_"}
+
             <p>Humidity</p>
           </div>
           <div className="wind">
-            {data.wind ? <p>{data.wind.speed}  </p> : ('_') }
+            {data.wind ? <p>{data.wind.speed} Km/h</p> : "_"}
             <p>Wind Speed</p>
           </div>
         </div>
